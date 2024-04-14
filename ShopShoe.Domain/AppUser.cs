ac63ca4ef2df1c91280c332129e2b2c2fdfb7b8f
tranducminh <minhk19caothang@gmail.com>
2024-04-14 17:32:18 +0700

    create database

---
+++ ShopShoe.Domain/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopShoe.Domain
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string FullName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ ShopShoe.Infastructure/DbContext/ShopShoeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopShoe.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopShoe.Infastructure.DbContext
+{
+    public class ShopShoeDbContext : IdentityDbContext<AppUser, AppRole,int>
+    {
+        public ShopShoeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<AppUser> AppUsers { get; set; }
+        DbSet<AppUser> AppRole { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+
+    }
+}
